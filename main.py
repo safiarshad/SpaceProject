d02730f41e7c25867a9824103174183a95ecaa25
safiarshad <ah.safi2001@gmail.com>
2024-01-18 23:09:05 -0500

    main function and display window added

--- main.py
+++ main.py
@@ -2,3 +2,28 @@ import pygame
 import math
 pygame.init()
 
+"""
+Setting the initial window display here
+
+"""
+WIDTH , HEIGHT = 800, 800 #in Capitals because they are constants
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Planet Simulation")
+
+#we need a loop to keep the program running 
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get(): #this keeps track and grabs a list of the events (clicks...) that happen in pygame
+           if event.type == pygame.QUIT:
+               run = False  
+    pygame.quit()
+
+
+main()
+
+
+
+
