6af1e5ffaa4c8565ad7ba6f1221770947a731bac
safiarshad <ah.safi2001@gmail.com>
2024-01-25 15:46:13 -0500

    Read me documentation: Math and Physics part

--- README.md
+++ README.md
@@ -7,6 +7,35 @@ different planets. Which would help in generating a more precise orbit for the p
 
 
 vel: Velocity in both x and y direction since the planet is moving in a circular motion 
+Those Vel are determined by the force of attraction of the force of gravity between the planets
+
+F = G * Mm/r**2  (r is the distance between the two objects in this case) but this formula gives us a sraight line force
+it has to be broken downt to x and y components. Pythagores THeorem would be use to calculate 'r'.
+              .
+           .  . 
+    F    .    . 
+       .      .   y and Fy(force component)
+     .        .   
+   .          .
+. ). . . . .  .       Theta is that angle = T which will be used in the calculation
+
+       x and Fx
+
+tanT = Opp/Adj == y/x
+
+tan**-1 (y/x) = T ==>> Value of the angel Theta 
+
+SOH
+CAH
+TOA
+
+SinT = Fy/F
+
+CosT = Fx/F ==> Fx= CosT * F
+
+
+
+
 
 the x and y are in meters
 
--- main.py
+++ main.py
@@ -11,10 +11,13 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 WHITE = (255, 255, 255) #r,g,b value
 YELLOW = (255, 255, 0)
+BLUE = (100, 149, 237) # 0,0,255 for a regular nice dark blue 
+RED = (188, 39, 50)
+DARK_GREY = (80, 78, 81)
 
 
 class Planet:
-    AU = 149.6e6 * 1000 #astronomical units and it is approx = dis of earth to sun by meter (hence the 1000x)
+    AU = 149.6e6 * 1000 #astronomical units and it is approx => dis of earth to sun by meter (hence the 1000x)
     G = 6.67428e-11
     SCALE = 250/AU  #scaling the orbits within the 800x800 frame pixels 1AU = 100pixels
     TIMESTEP = 3600*24 #1day of timelapse
@@ -56,7 +59,12 @@ def main():
     sun = Planet(0, 0 , 30, YELLOW, 1.98892 * 10**30) 
     sun.sun = True 
 
-    planets = [sun]
+    earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24) # -1 for the left and 1 for the right side of AU
+    mars = Planet(-1.524 * Planet.AU, 0,12, RED, 6.39 * 10**23)
+    mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
+    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24 )
+
+    planets = [sun, earth, mars, mercury, venus]
 
     while run:
         clock.tick(60) #the loop will run a maximum of 60 times per sec
@@ -64,18 +72,20 @@ def main():
         #WIN.fill(WHITE) #fills the whole screen 
         #pygame.display.update() #kinda self explanatory (updates the drawings added)
 
-        for event in pygame.event.get(): #this keeps track and grabs a list of the events (clicks...) that happen in pygame
+        for event in pygame.event.get(): #this keeps track and grabs a list of the events that happen in pygame
            if event.type == pygame.QUIT:
                run = False  
 
         for planet in planets:
-            planet.draw(WIN)
+            planet.draw(WIN) #the argument is the Window that we wanna draw on
 
         pygame.display.update()
     pygame.quit()
 
 
-main()
+
+if __name__ == "__main__":
+    main()
 
 
 
