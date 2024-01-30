8d0baffc5883817edc223fc27847ba3397ba8b68
safiarshad <ah.safi2001@gmail.com>
2024-01-29 20:01:33 -0500

    Changed the size of the planets

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ pygame.init()
 Setting the initial window display here
 
 """
-WIDTH , HEIGHT = 800, 800 #in Capitals because they are constants
+WIDTH , HEIGHT = 1000, 800 #in Capitals because they are constants
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
@@ -109,19 +109,19 @@ def main():
     """
     clock = pygame.time.Clock()
 
-    sun = Planet(0, 0 , 30, YELLOW, 1.98892 * 10**30) 
+    sun = Planet(0, 0 , 40, YELLOW, 1.98892 * 10**30) 
     sun.sun = True 
 
-    earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
+    earth = Planet(-1 * Planet.AU, 0, 22, BLUE, 5.9742 * 10**24)
     earth.y_vel = 29.783 * 1000 
 
-    mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+    mars = Planet(-1.524 * Planet.AU, 0, 18, RED, 6.39 * 10**23)
     mars.y_vel = 24.077 * 1000
 
-    mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
+    mercury = Planet(0.387 * Planet.AU, 0, 14, DARK_GREY, 3.30 * 10**23)
     mercury.y_vel = -47.4 * 1000
 
-    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
+    venus = Planet(0.723 * Planet.AU, 0, 20, WHITE, 4.8685 * 10**24)
     venus.y_vel = -35.02 * 1000
 
     planets = [sun, earth, mars, mercury, venus]
